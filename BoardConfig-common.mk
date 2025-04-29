5db1cccfeff02f8b5e971b514f3d7deef46e082c
Daniel Micay <daniel.micay@grapheneos.org>
2025-04-29 17:55:44 -0400

    switch to synchronous kernel MTE

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -45,7 +45,7 @@ BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 
 # Normal (non-_fullmte) builds
 ifeq (,$(filter %_fullmte,$(TARGET_PRODUCT)))
-BOARD_KERNEL_CMDLINE += kasan.fault=panic kasan.mode=asymm
+BOARD_KERNEL_CMDLINE += kasan.fault=panic
 endif
 
 BOARD_BOOTCONFIG += androidboot.boot_devices=13200000.ufs
