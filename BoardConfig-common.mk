ec80d99410f2460cd65487caaccc335e6410ea79
Daniel Micay <daniel.micay@grapheneos.org>
2025-04-29 17:55:44 -0400

    switch to synchronous kernel MTE

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -38,7 +38,7 @@ BOARD_KERNEL_CMDLINE += \
     disable_dma32=on \
     sysctl.kernel.sched_pelt_multiplier=4 \
     rodata=on \
-    kasan.fault=panic kasan.mode=asymm \
+    kasan.fault=panic \
     at24.write_timeout=100 \
     log_buf_len=1024K \
     android_arch_task_struct_size=512
