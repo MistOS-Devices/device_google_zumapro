Trijal08/android_device_google_zuma

Include extras such as BCR, PixelParts, Pixel Face Unlock, Pixel camera, and ViPER4AndroidFX

extras.mk
@@ -1,0 +1,20 @@
+## Definitions
+# DeviceAsWebcam
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
+## Include extras
+# PixelParts
+$(call inherit-product-if-exists, packages/apps/PixelParts/device.mk)
+
+# Google Face Unlock
+$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
+
+# Google Pixel Camera
+$(call inherit-product-if-exists, vendor/google/camera/camera.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+# BCR (Basic Call Recorder)
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)

lineage_common.mk
@@ -56,3 +56,6 @@ include hardware/google/pixel/touch/device.mk
 
 # UDFPS
 TARGET_HAS_UDFPS := true
+
+# Include some extras if they exist
+-include device/google/zuma/extras.mk

MistOS-Devices/device_google_zumapro

extras.mk
@@ -1,0 +1,20 @@
+## Definitions
+# DeviceAsWebcam
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
+## Include extras
+# PixelParts
+$(call inherit-product-if-exists, packages/apps/PixelParts/device.mk)
+
+# Google Face Unlock
+$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
+
+# Google Pixel Camera
+$(call inherit-product-if-exists, vendor/google/camera/config.mk)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+# BCR (Basic Call Recorder)
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)

lineage_common.mk
@@ -49,3 +49,6 @@ include hardware/google/pixel/touch/device.mk
 
 # UDFPS
 TARGET_HAS_UDFPS := true
+
+# Include some extras if they exist
+-include device/google/zumapro/extras.mk